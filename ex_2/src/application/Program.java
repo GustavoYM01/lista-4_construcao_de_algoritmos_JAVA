2976d8b1c7c970fa89c0d7612a675edc26f8166f
Gustavo <69603394+GustavoYM01@users.noreply.github.com>
2021-10-26 22:24:27 -0300

    Update Program.java

--- ex_2/src/application/Program.java
+++ ex_2/src/application/Program.java
@@ -14,35 +14,9 @@ public class Program {
 		int totalVotesForCandidate4 = 0;
 		int totalNullVotes = 0;
 		int totalBlankVotes = 0;
+		int numberChoice = 0;
 
-		System.out.println("Digite: 1 - candidato 1 | 2 - candidato 2 | "
-				+ "3 - candidato 3 | 4 - candidato 4 | 5 - voto nulo | " + "6 - voto em branco");
-		int numberChoice = sc.nextInt();
-		sc.nextLine();
-		
-		if(numberChoice <= 0 || numberChoice > 6) {
-			System.out.printf("Opção inválida.%n");
-		}
-		else if(numberChoice == 1) {
-			totalVotesForCandidate1 += 1;
-		}
-		else if (numberChoice == 2) {
-			totalVotesForCandidate2 += 1;
-		}
-		else if (numberChoice == 3) {
-			totalVotesForCandidate3 += 1;
-		}
-		else if (numberChoice == 4) {
-			totalVotesForCandidate4 += 1;
-		}
-		else if (numberChoice == 5) {
-			totalNullVotes += 1;
-		}
-		else {
-			totalBlankVotes += 1;
-		}
-		
-		while(numberChoice != 0) {
+		do {
 			System.out.println("Digite: 1 - candidato 1 | 2 - candidato 2 | "
 					+ "3 - candidato 3 | 4 - candidato 4 | 5 - voto nulo | " + "6 - voto em branco");
 			numberChoice = sc.nextInt();
@@ -70,7 +44,8 @@ public class Program {
 			else if(numberChoice == 6){
 				totalBlankVotes += 1;
 			}
-		}
+		}while(numberChoice != 0);
+			
 		
 		System.out.printf("Total de votos para o candidato 1: %d%n", totalVotesForCandidate1);
 		System.out.printf("Total de votos para o candidato 2: %d%n", totalVotesForCandidate2);
