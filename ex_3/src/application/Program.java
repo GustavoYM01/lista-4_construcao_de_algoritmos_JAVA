c932a9eba9a0b56b8dc1d990c562633e4618a635
Gustavo <69603394+GustavoYM01@users.noreply.github.com>
2021-10-26 22:16:39 -0300

    Update Program.java

--- ex_3/src/application/Program.java
+++ ex_3/src/application/Program.java
@@ -14,55 +14,60 @@ public class Program {
 		System.out.print("Digite o código do aluno: ");
 		int studentCode = sc.nextInt();
 		
-		while(studentCode != 0) {
-			
-			System.out.print("Digite a 1° nota do aluno: ");
-			notes = sc.nextDouble();
-			sc.nextLine();
-			
-			if(notes < 0 || notes > 10) {
-				System.out.print("Nota inválida");
-				break;
+		if(studentCode < 0 || studentCode == 0) {
+			System.out.print("Código inválido.");
+		}
+		else {
+			while(studentCode != 0) {
+				
+				System.out.print("Digite a 1° nota do aluno: ");
+				notes = sc.nextDouble();
+				sc.nextLine();
+				
+				if(notes < 0 || notes > 10) {
+					System.out.print("Nota inválida");
+					break;
+				}
+				else {
+					sumNotes += notes;
+				}
+				
+				System.out.print("Digite a 2° nota do aluno: ");
+				notes = sc.nextDouble();
+				sc.nextLine();
+				
+				if(notes < 0 || notes > 10) {
+					System.out.print("Nota inválida");
+					break;
+				}
+				else {
+					sumNotes += notes;
+				}
+				
+				System.out.print("Digite a 3° nota do aluno: ");
+				notes = sc.nextDouble();
+				sc.nextLine();
+				
+				if(notes < 0 || notes > 10) {
+					System.out.print("Nota inválida");
+					break;
+				}
+				else {
+					sumNotes += notes;
+				}
+				
+				avgNotes = sumNotes / 3.0;
+				
+				System.out.printf("Média das notas do aluno %d: %.2f%n", studentCode, avgNotes);
+				
+				System.out.println();
+				
+				sumNotes = 0.0;
+				avgNotes = 0.0;
+				
+				System.out.print("Digite outro código de aluno: ");
+				studentCode = sc.nextInt();
 			}
-			else {
-				sumNotes += notes;
-			}
-			
-			System.out.print("Digite a 2° nota do aluno: ");
-			notes = sc.nextDouble();
-			sc.nextLine();
-			
-			if(notes < 0 || notes > 10) {
-				System.out.print("Nota inválida");
-				break;
-			}
-			else {
-				sumNotes += notes;
-			}
-			
-			System.out.print("Digite a 3° nota do aluno: ");
-			notes = sc.nextDouble();
-			sc.nextLine();
-			
-			if(notes < 0 || notes > 10) {
-				System.out.print("Nota inválida");
-				break;
-			}
-			else {
-				sumNotes += notes;
-			}
-			
-			avgNotes = sumNotes / 3.0;
-			
-			System.out.printf("Média das notas do aluno %d: %.2f%n", studentCode, avgNotes);
-			
-			System.out.println();
-			
-			sumNotes = 0.0;
-			avgNotes = 0.0;
-			
-			System.out.print("Digite outro código de aluno: ");
-			studentCode = sc.nextInt();
 		}
 		
 		sc.close();
